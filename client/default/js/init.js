22dc1587e1ab9d3676625b499e4744ffba842ad2
michael.hearne@feedhenry.com <michael.hearne@feedhenry.com>
2014-05-29 10:55:00 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -7,20 +7,10 @@ call which will return the remote config.
 */
 
 $fh.ready(function() {
-  //$fh.init(config);
-  
-  $fh.init(config, function(res) {
-  // Init call was successful. Alert the response
-  alert('Got response from init:' + JSON.stringify(res));
-}, function(msg, err) {
-  // An error occured during the init call. Alert some debugging information
-  alert('Init call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
-  });
-  
-  // The local config variable from config.js can be accessed directly
- // document.getElementById('localConfig').innerHTML = "<p>" + JSON.stringify(config) + "</p>";
+ // The local config variable from config.js can be accessed directly
+ document.getElementById('localConfig').innerHTML = "<p>" + JSON.stringify(config) + "</p>";
 
-  //document.getElementById('run_button').onclick = function() {
+  document.getElementById('run_button').onclick = function() {
     // Invoke a cloud action call to get the remote configuration
     // See: http://docs.feedhenry.com/wiki/Actions
       /*
@@ -36,5 +26,5 @@ $fh.ready(function() {
       }
     );
     */
-  //};
+  };
 });
